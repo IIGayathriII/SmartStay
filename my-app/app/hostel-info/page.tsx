5a293e1422e481e79c8b19e4244c6113be93e683
Gayathri Asok <112485894+IIGayathriII@users.noreply.github.com>
2025-03-25 19:24:15 +0530

    Delete my-app directory

--- my-app/app/hostel-info/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import HostelPage from "../components/ui/airbnb";
-export default function AirbnbDemo() {  
-    return (
-        <>
-        <HostelPage />
-        </>
-    );
-    }   
